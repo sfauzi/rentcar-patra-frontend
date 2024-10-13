1d26706203badba5666966b2d31b8ae74843ac9c
sfauzii <sjahrulfauzii@gmail.com>
2024-10-13 14:02:51 +0700

    feat(index): section faq created

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,26 +1,44 @@
-const navbar = document.querySelector('.navbar');
-let lastScrollTop = 0; // Variabel untuk menyimpan posisi scroll terakhir
-const delta = 5; // Jumlah piksel untuk deteksi scroll
+const navbar = document.querySelector('.navbar')
+let lastScrollTop = 0 // Variabel untuk menyimpan posisi scroll terakhir
+const delta = 5 // Jumlah piksel untuk deteksi scroll
 
 window.addEventListener('scroll', () => {
-    const currentScroll = window.scrollY;
+  const currentScroll = window.scrollY
 
-    // Menangani navbar blur saat di-scroll
-    if (currentScroll > 50) {
-        navbar.classList.add('scrolled');
+  // Menangani navbar blur saat di-scroll
+  if (currentScroll > 50) {
+    navbar.classList.add('scrolled')
 
-        // Menyembunyikan navbar saat menggulir ke bawah
-        if (Math.abs(currentScroll - lastScrollTop) >= delta) {
-            if (currentScroll > lastScrollTop) {
-                navbar.style.transform = 'translateY(-100%)'; // Menyembunyikan navbar
-            } else {
-                navbar.style.transform = 'translateY(0)'; // Menampilkan navbar
-            }
-        }
-    } else {
-        navbar.classList.remove('scrolled');
-        navbar.style.transform = 'translateY(0)'; // Pastikan navbar terlihat saat di atas
+    // Menyembunyikan navbar saat menggulir ke bawah
+    if (Math.abs(currentScroll - lastScrollTop) >= delta) {
+      if (currentScroll > lastScrollTop) {
+        navbar.style.transform = 'translateY(-100%)' // Menyembunyikan navbar
+      } else {
+        navbar.style.transform = 'translateY(0)' // Menampilkan navbar
+      }
     }
+  } else {
+    navbar.classList.remove('scrolled')
+    navbar.style.transform = 'translateY(0)' // Pastikan navbar terlihat saat di atas
+  }
 
-    lastScrollTop = currentScroll; // Update posisi scroll terakhir
-});
+  lastScrollTop = currentScroll // Update posisi scroll terakhir
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  var faqCards = document.querySelectorAll('.faq-card')
+
+  faqCards.forEach(function (card) {
+    card.addEventListener('click', function () {
+      var answer = this.querySelector('.faq-answer')
+
+      if (this.classList.contains('active')) {
+        this.classList.remove('active')
+        answer.style.maxHeight = null
+      } else {
+        this.classList.add('active')
+        answer.style.maxHeight = answer.scrollHeight + 'px'
+      }
+    })
+  })
+})
