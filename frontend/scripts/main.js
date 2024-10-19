b5a6f9cc7131adb0f143fd300be158c5d8849ad4
sfauzii <sjahrulfauzii@gmail.com>
2024-10-19 14:28:56 +0700

    refactor(details): fixed page details

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -42,3 +42,68 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   })
 })
+
+
+// scroll button details car
+document.addEventListener('DOMContentLoaded', function () {
+  const leftButton = document.querySelector('.scroll-button.left')
+  const rightButton = document.querySelector('.scroll-button.right')
+  const scrollContainer = document.querySelector('.grid-images')
+
+  leftButton.addEventListener('click', function () {
+    scrollContainer.scrollBy({
+      left: -200 /* Adjust this value to control the scroll distance */,
+      behavior: 'smooth'
+    })
+  })
+
+  rightButton.addEventListener('click', function () {
+    scrollContainer.scrollBy({
+      left: 200 /* Adjust this value to control the scroll distance */,
+      behavior: 'smooth'
+    })
+  })
+})
+// Modal functions
+function openModal (imageSrc) {
+  document.getElementById('imageModal').style.display = 'flex' // Changed to 'flex' for centering
+  document.getElementById('modalImage').src = imageSrc
+}
+
+function closeModal () {
+  document.getElementById('imageModal').style.display = 'none'
+}
+
+// Close modal when clicking anywhere outside of the image
+window.onclick = function (event) {
+  if (event.target == document.getElementById('imageModal')) {
+    closeModal()
+  }
+}
+
+
+// JavaScript for tab switching
+document.addEventListener("DOMContentLoaded", function() {
+  const tabButtons = document.querySelectorAll('.tab-btn');
+  const tabContents = document.querySelectorAll('.tab-content');
+
+  // Ensure the first tab content is visible by default
+  tabContents[0].style.display = 'block';
+
+  tabButtons.forEach(button => {
+      button.addEventListener('click', function() {
+          // Remove active class from all buttons
+          tabButtons.forEach(btn => btn.classList.remove('active'));
+
+          // Add active class to the clicked button
+          this.classList.add('active');
+
+          // Hide all content
+          tabContents.forEach(content => content.style.display = 'none');
+
+          // Show the corresponding content
+          const tabId = this.getAttribute('data-tab');
+          document.getElementById(tabId).style.display = 'block';
+      });
+  });
+});
